1c406d52feca745b467a9b496f4797c434985d38
Peter Hodges <dr.pj.hodges@gmail.com>
2013-12-07 15:46:10 +0000

    Show how to create a line of fixed length, knowing the starting position and direction

---
+++ Questions/591101/Scripts/PlayerBall.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+var target : GameObject;
+
+var speedX : float = 1.0f / 10.0f;
+var speedScaleZ : float = 4.0f;
+var motionScale : float = 10.0f;
+
+var lineRender : LineRenderer;
+
+function Start () {
+	lineRender = GetComponent(LineRenderer);
+}
+
+function Update () {
+	var position = transform.position;
+	position.x = Mathf.Sin(speedX * Time.time) * motionScale;
+	position.z = Mathf.Sin(speedX * speedScaleZ * Time.time) * motionScale;
+	transform.position = position;
+	
+	var direction = Vector3(Mathf.Cos(speedX * Time.time), 0.0f, Mathf.Cos(speedX * speedScaleZ * Time.time) * speedScaleZ);
+	transform.rotation = Quaternion.LookRotation(direction);
+	
+	if (null != target && null != lineRender) {
+		lineRender.SetPosition(0, transform.position);
+		lineRender.SetPosition(1, target.transform.position);
+	}
+}
---
+++ Questions/591101/Scripts/TargetBall.js
@@ -0,0 +1,41 @@
+﻿#pragma strict
+
+var player : GameObject;
+
+var lineRender : LineRenderer;
+
+var lineLength : float = 2.0f;
+
+function Start () {
+	lineRender = GetComponent(LineRenderer);
+
+
+}
+
+function Update () {
+	if (null != player && null != lineRender) {
+		var approaching : boolean = false;
+		
+		// Test whether the vector from the player to the target has a component along 
+		// its direction of travel.
+		var separation : Vector3 = transform.position - player.transform.position;
+		if (0.0f < Vector3.Dot(separation, player.transform.forward)) {
+			approaching = true;
+		}
+		
+		// We only draw something if the player is not moving away from the target.
+		if (approaching) {
+			// Create a line starting at the target and lying along the direction of the separation vector
+			var tangent : Vector3 = Vector3.Normalize(separation) * lineLength;
+			
+			var start : Vector3 = transform.position;
+			var end : Vector3 = start + tangent;
+			
+			lineRender.SetPosition(0, start);
+			lineRender.SetPosition(1, end);
+			lineRender.enabled = true;
+		} else {
+			lineRender.enabled = false;
+		}
+	}
+}
